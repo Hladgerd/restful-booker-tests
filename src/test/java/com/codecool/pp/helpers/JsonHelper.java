0ca3b26676c05646ebd3fb738dc7aade2b465415
Hladgerd <hladgerd@gmail.com>
2024-01-18 20:51:24 +0100

    add helper class to collect json keys of booking data

---
+++ src/test/java/com/codecool/pp/helpers/JsonHelper.java
@@ -0,0 +1,19 @@
+package com.codecool.pp.helpers;
+
+import java.util.List;
+
+public class JsonHelper {
+    public static final String FIRSTNAME = "firstname";
+    public static final String LASTNAME = "lastname";
+    public static final String TOTAL_PRICE = "totalprice";
+    public static final String DEPOSIT_PAID = "depositpaid";
+    public static final String CHECKIN = "checkin";
+    public static final String CHECKOUT = "checkout";
+    public static final String ADDITIONAL_NEEDS = "additionalneeds";
+    public static final String BOOKING_DATES = "bookingdates";
+
+    public static final String BOOKING_ID = "bookingid";
+
+    public static final List<String> JSONKEYS = List.of(FIRSTNAME, LASTNAME, TOTAL_PRICE, DEPOSIT_PAID,
+            BOOKING_DATES + "." + CHECKIN, BOOKING_DATES + "." + CHECKOUT, ADDITIONAL_NEEDS);
+}
