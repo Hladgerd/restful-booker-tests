72d747c1e37d53e9f4cfa8ac8e5b8a01366ba237
Hladgerd <hladgerd@gmail.com>
2024-01-18 23:55:59 +0100

    add post request for booking creation

---
+++ src/test/java/com/codecool/pp/requests/PostBookingRequest.java
@@ -0,0 +1,22 @@
+package com.codecool.pp.requests;
+
+import com.codecool.pp.ApiUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class PostBookingRequest {
+
+    public static Response createBooking(String bookingLoad){
+        return given()
+                .contentType(ContentType.JSON)
+                .accept("application/json")
+                .body(bookingLoad)
+                .when()
+                .post(ApiUrl.createBookingUrl())
+                .then()
+                .extract()
+                .response();
+    }
+}
