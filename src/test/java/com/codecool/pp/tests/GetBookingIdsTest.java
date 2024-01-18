782bbb9d5fd8349ef78e94997d2d620f20137ab7
Hladgerd <hladgerd@gmail.com>
2024-01-18 20:52:47 +0100

    add tests to search for booking by ID

--- src/test/java/com/codecool/pp/tests/GetBookingIdsTest.java
+++ src/test/java/com/codecool/pp/tests/GetBookingIdsTest.java
@@ -1,6 +1,8 @@
 package com.codecool.pp.tests;
 
 import com.codecool.pp.requests.GetRequest;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,10 +12,7 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.*;
 
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-
-
+import static com.codecool.pp.helpers.JsonHelper.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GetBookingIdsTest {
@@ -26,12 +25,12 @@ public class GetBookingIdsTest {
     }
 
     @Test
-    @DisplayName("Get all bookings and verify we get result")
+    @DisplayName("Get all bookings and verify that we get result")
     void getBookingsTest() {
 
         Response allBookingsResponse = GetRequest.getAllBookings();
         JsonPath jsonPath = allBookingsResponse.jsonPath();
-        List<Integer> bookingIds = jsonPath.getList("bookingid");
+        List<Integer> bookingIds = jsonPath.getList(BOOKING_ID);
 
         assertThat(allBookingsResponse.statusCode()).isEqualTo(HttpStatus.SC_OK);
         assertThat(bookingIds).isNotEmpty();
@@ -41,10 +40,7 @@ public class GetBookingIdsTest {
     @DisplayName("Get all bookings and verify their IDs are all unique")
     void getBookingIdsTest() {
 
-        Response allBookingsResponse = GetRequest.getAllBookings();
-        JsonPath jsonPath = allBookingsResponse.jsonPath();
-        List<Integer> bookingIds = jsonPath.getList("bookingid");
-
+        List<Integer> bookingIds = getAllBookingIds();
         int bookingIdsSize = bookingIds.size();
         long uniqueIds = bookingIds.stream()
                 .distinct()
@@ -54,27 +50,82 @@ public class GetBookingIdsTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/bookingFilters.csv", numLinesToSkip = 1, delimiter = ';')
+    @CsvFileSource(resources = "/bookingValidFilters.csv", numLinesToSkip = 1, delimiter = ';')
     @DisplayName("Get filtered bookings and verify the randomly picked booking matches the filter")
     void getFilteredBookingsTest(String key, String value) {
 
         queryParameter.put(key, value);
         Response filteredBookingsResponse = GetRequest.getFilteredBookings(queryParameter);
         JsonPath filteredBookingsJsonPath = filteredBookingsResponse.jsonPath();
-        List<Integer> bookingIds = filteredBookingsJsonPath.getList("bookingid");
+        List<Integer> bookingIds = filteredBookingsJsonPath.getList(BOOKING_ID);
 
         JsonPath pickedBooking = pickRandomBooking(bookingIds);
 
         if (key.contains("check")) {
-            assertThat(pickedBooking.getString("bookingdates" + "." + key)).isGreaterThanOrEqualTo(value);
+            assertThat(pickedBooking.getString(BOOKING_DATES + "." + key)).isGreaterThanOrEqualTo(value);
         } else {
             assertThat(pickedBooking.getString(key)).isEqualTo(value);
         }
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/bookingInvalidFilters.csv", numLinesToSkip = 1, delimiter = ';')
+    @DisplayName("Get bookings by invalid filter and verify that no data was found")
+    void getBookingsByInvalidFilterTest(String key, String value) {
+
+        queryParameter.put(key, value);
+        Response filteredBookingsResponse = GetRequest.getFilteredBookings(queryParameter);
+        String bookingData = filteredBookingsResponse.getBody().asString();
+
+        assertThat(filteredBookingsResponse.statusCode()).isEqualTo(HttpStatus.SC_OK);
+        assertThat(bookingData).isEqualTo("[]");
+    }
+
+    @Test
+    @DisplayName("Get booking by id and verify that we get result")
+    void getBookingByIdTest() {
+
+        int bookingId = pickValidId();
+        Response getBookingResponse = GetRequest.getBookingById(bookingId);
+        JsonPath getBookingJsonPath = getBookingResponse.jsonPath();
+
+        assertThat(getBookingResponse.statusCode()).isEqualTo(HttpStatus.SC_OK);
+        for (String jsonkey: JSONKEYS) {
+            assertThat(getBookingJsonPath.getString(jsonkey)).isNotEmpty();
+        }
+    }
+
+    @Test
+    @DisplayName("Get booking by invalid id and verify that no booking was found")
+    void getBookingByInvalidIdTest() {
+
+        int bookingId = pickInvalidId();
+        Response getBookingResponse = GetRequest.getBookingById(bookingId);
+
+        assertThat(getBookingResponse.statusCode()).isEqualTo(HttpStatus.SC_NOT_FOUND);
+    }
+
+    private List<Integer> getAllBookingIds() {
+        Response allBookingsResponse = GetRequest.getAllBookings();
+        JsonPath jsonPath = allBookingsResponse.jsonPath();
+        return jsonPath.getList("bookingid");
     }
 
     private JsonPath pickRandomBooking(List<Integer> bookingIds) {
         Collections.shuffle(bookingIds);
         Response bookingByIdResponse = GetRequest.getBookingById(bookingIds.get(0));
         return bookingByIdResponse.jsonPath();
+    }
+
+    private int pickValidId() {
+        List<Integer> bookingIds = getAllBookingIds();
+        Collections.shuffle(bookingIds);
+        return bookingIds.get(0);
+    }
+
+    private int pickInvalidId() {
+        List<Integer> bookingIds = getAllBookingIds();
+        Collections.sort(bookingIds);
+        return bookingIds.get((bookingIds.size()-1)) + 1;
     }
 }
